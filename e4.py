53568315a57e2e157b26b6f3e6e800da8de7fab4
Henrique Simoncini <ph.simoncini@gmail.com>
2026-05-28 19:35:36 -0300

    Update e4.py

--- e4.py
+++ e4.py
@@ -1,1 +1,11 @@
-
+import random
+numero_secreto = random.randint(1, 100)
+tentativa = 0
+while tentativa != numero_secreto:
+    tentativa = int(input("Digite um número entre 1 e 100: "))
+    if tentativa > numero_secreto:
+        print("Muito alto!")
+    elif tentativa < numero_secreto:
+        print("Muito baixo!")
+    else:
+        print("Parabéns! Você acertou!")
