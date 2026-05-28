acb102ef2a75c29110780860a3f61893b512ac46
Henrique Simoncini <ph.simoncini@gmail.com>
2026-05-28 19:33:25 -0300

    Update e3.py

--- e3.py
+++ e3.py
@@ -1,1 +1,12 @@
-
+senha = input("Digite a senha: ")
+tem_numero = False
+tem_letra = False
+for caractere in senha:
+    if caractere.isdigit():
+        tem_numero = True
+    if caractere.isalpha():
+        tem_letra = True
+if len(senha) >= 8 and tem_numero and tem_letra:
+    print("Senha válida")
+else:
+    print("Senha inválida")
