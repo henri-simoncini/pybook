2391dcb5f153b1a3a3b8750bf1524aaca3d04022
Henrique Simoncini <ph.simoncini@gmail.com>
2026-05-28 18:39:33 -0300

    Update d5.py

--- d5.py
+++ d5.py
@@ -1,1 +1,6 @@
-
+lista = input("Digite os números separados por espaço: ").split()
+valor = input("Digite o valor que deseja buscar: ")
+if valor in lista:
+    print("Valor encontrado")
+else:
+    print("Valor não encontrado")
